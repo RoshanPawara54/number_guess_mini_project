62fcf4caed69f226711b9f8b9e235b6003f1bcc9
RoshanPawara54 <roshanpawara335@gmail.com>
2025-08-29 13:22:27 +0530

    number guessing game

---
+++ main.py
@@ -0,0 +1,82 @@
+import random
+import time
+import sys
+
+def loading():
+  for i in range(4):
+    sys.stdout.write(".")
+    sys.stdout.flush()
+    time.sleep(0.5)
+  print()
+
+def pr():
+  for i in range(10):
+    sys.stdout.write("oh")
+    sys.stdout.flush()
+    time.sleep(0.2)
+  print()
+
+def number_guessing_game():
+  print("Choose Tum konsi level khelna chahate ho: \n1. Level Easy: 10 attempt\n2. Level Medium: 7 attempt\n3. Level Hard :5 attempt")
+  while True:
+    ch=(int(input("\n1.,2.,3...?:")))
+    if ch==1:
+      attempt_left=10
+      break
+    elif ch==2:
+      attempt_left=7
+      break
+    elif ch==3:
+      attempt_left=5
+      break
+    else:
+      print("\noption diya hai usme se select kro....")
+  select=random.randint(1,100)
+  print("Ruko jara Sabar karo, Secret Number Sochne Do")
+  loading()
+  print("Soch liya..")
+  print(select)
+
+  if attempt_left==5:
+    while True:
+      a=input("\nHint chahiye..?(ha/na)")
+      if a=="ha":
+        if select<50:
+          print("Number 51 se chota hai..")
+          break
+        else:
+          print("Number 50 se bada Hai..")
+          break
+      elif a=="na":
+        print("okk,Agee badte hai.")
+        break
+      else:
+        print("ha ya na..?")
+
+  while attempt_left>0:
+    try:
+      guess=int(input("\nGuess Batao 1 to 100 :"))
+      if guess==select:
+        print(attempt_left)
+        if attempt_left in (10,7,5):
+          pr()
+          print("7 cr....")
+          time.sleep(1)
+          print("\nSahi javab in 1st try. Attempt remaining = ",attempt_left," Secret Number = ",select)
+        else:
+          print("\nCongrats.. Sahi guess. Attempt remaining = ",attempt_left," Secret Number = ",select)
+        break
+      elif guess < select:
+        remain=attempt_left-1
+        print("\nBahut chota number Guess kiya. Attempt remaining = ",remain)
+      else:
+        remain=attempt_left-1
+        print("\nBahut Bada nnmber Guess Kiya. Attempt remaining = ",remain)
+      attempt_left -= 1
+    except ValueError:
+      print("1 to 100 ke bich mese batao..!")
+  
+  if attempt_left==0:
+    print("\nOps.. Sab attempt khatam Hogaye. Attempt remaining = ",attempt_left," Secret Number was = ",select)
+  time.sleep(2)
+number_guessing_game()
